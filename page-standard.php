991c1de8430e1c3940fd20547234675c0c3c1087
Sam Vipond <sammy.potato@gmail.com>
2017-06-10 12:39:10 -0400

    add gif field to standard page

--- page-standard.php
+++ page-standard.php
@@ -2,7 +2,9 @@
 /*
 Template Name: Standard Page 
  */
-get_header(); ?>
+get_header(); 
+$gif = get_field('gif');
+?>
 
 <h1 class="post-title entry-title"><?php echo the_title(); ?></h1>
 
@@ -12,7 +14,14 @@ get_header(); ?>
 					
 	<?php the_content(); ?>
 	
-<?php endwhile; ?> 
+<?php endwhile; 
+
+if($gif){ ?>
+	 <img src="<?php echo $gif['url']; ?>" alt="gif" class="gif">
+<?php } ?> 
+
+
+
 
 
 </div>
