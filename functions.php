5750f11093c7099cfa4852736a4550bf8d2b5538
Sam Vipond <sammy.potato@gmail.com>
2017-08-17 13:06:09 -0400

    customizations

--- functions.php
+++ functions.php
@@ -4,4 +4,9 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
-?>
+
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page('Fonts & Colours');
+	
+}
--- header.php
+++ header.php
@@ -6,8 +6,69 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<link rel="stylesheet" href="<?=get_stylesheet_directory_uri()?>/assets/css/style.min.css" />
+	<?php if(get_field('title_font', 'option')) : ?>
+	<link href="https://fonts.googleapis.com/css?family=<?php the_field('title_font', 'option'); ?>" rel="stylesheet">
+	<?php endif; ?>
+	<?php if(get_field('menu_font', 'option')) : ?>
+	<link href="https://fonts.googleapis.com/css?family=<?php the_field('menu_font', 'option'); ?>" rel="stylesheet">
+	<?php endif; ?>
+	<?php if(get_field('body_font', 'option')) : ?>
+	<link href="https://fonts.googleapis.com/css?family=<?php the_field('body_font', 'option'); ?>" rel="stylesheet">
+	<?php endif; ?>
 	<?php wp_head(); ?>
 </head>
+
+<style>
+
+	/* BODY */
+	<?php if(get_field('body_font', 'option')) : ?>
+	body{
+		font-family: <?php the_field('body_font', 'option'); ?>, <?php the_field('menu_fallback', 'option'); ?>;
+	}
+	<?php endif; ?>
+
+	<?php if(get_field('body_size', 'option')) : ?>
+	body{
+		font-size: <?php the_field('body_size', 'option'); ?>px;
+	}
+	<?php endif; ?>
+
+	/* SITE TITLE */
+	<?php if(get_field('title_font', 'option')) : ?>
+	.site-title a{
+		font-family: <?php the_field('title_font', 'option'); ?>, <?php the_field('title_fallback', 'option'); ?>;
+	}
+	<?php endif; ?>
+
+	<?php if(get_field('title_size', 'option')) : ?>
+	.site-title a{
+		font-size: <?php the_field('title_size', 'option'); ?>px;
+	}
+	<?php endif; ?>
+
+	/* MENU */
+	<?php if(get_field('menu_font', 'option')) : ?>
+	.menu-item a{
+		font-family: <?php the_field('menu_font', 'option'); ?>, <?php the_field('menu_fallback', 'option'); ?>;
+	}
+	<?php endif; ?>
+
+	<?php if(get_field('menu_size', 'option')) : ?>
+	.menu-item a, li.menu-item a{
+		font-size: <?php the_field('menu_size', 'option'); ?>px !important;
+	}
+	<?php endif; ?>
+
+	/* HOVER COLOR */
+	<?php if(get_field('hover_color', 'option')) : ?>
+	.menu-item a:hover, .sub-menu > .menu-item a:hover, .copyright a:hover{
+		color: <?php the_field('hover_color', 'option'); ?>!important;
+	}
+	<?php endif; ?>
+
+	
+
+</style>
 
 <body class="<?php hybrid_body_class(); ?>">
 
